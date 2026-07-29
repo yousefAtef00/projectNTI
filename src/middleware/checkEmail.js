bce0726591f09ca24b075c3b342c4e2a343d968e
yousefAtef00 <yatef8622@example.com>
2026-07-29 15:20:21 +0300

    Remove duplicate backend files from root

--- src/middleware/checkEmail.js
+++
@@ -1,8 +0,0 @@
-import { userModel } from "../../db/models/user.model.js"
-
-
-export const checkEmail=async (req,res,next)=>{
-    let exists = await userModel.findOne({email:req.body.email})
-    if(exists) return res.json({message:"user already registered, please login"})
-        next()
-}
--- src/modules/order/order.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import { verifyToke } from "../../middleware/verifyToken.js"
-import { checkout, getAllOrderForUser, getOrderById } from "./order.controller.js"
-
-export const orderRoutes = express().router
-orderRoutes.use(express.json())
-orderRoutes.get("/orders",verifyToke,getAllOrderForUser)
-orderRoutes.post("/cheakout",verifyToke,checkout)
-orderRoutes.get("/orders/:id",verifyToke,getOrderById)
